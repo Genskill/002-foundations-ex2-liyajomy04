b2e29877e9e3033046c6c5eb0828c74adeee9402
liyajomy04 <57555095+liyajomy04@users.noreply.github.com>
2021-05-18 23:07:36 +0530

    pythagorean.c

--- pythagorean.c
+++ pythagorean.c
@@ -3,9 +3,9 @@
 
 int main(void)
 {
-  int a = get_int("side 1\n");
-  int b = get_int("side 2\n");
-  int c = get_int("side 3\n");
+  int a = get_int("side 1 \n");
+  int b = get_int("side 2 \n");
+  int c = get_int("side 3 \n");
   int(sum) = (int)(a*a)+(b*b);
    if((c*c) >= sum)
    {
