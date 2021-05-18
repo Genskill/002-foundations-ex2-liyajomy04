cb69149993b34daaa3cece795f9ca0e517154090
liyajomy04 <57555095+liyajomy04@users.noreply.github.com>
2021-05-18 22:55:10 +0530

    pythagorean.c

---
+++ pythagorean.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+  int a = get_int("side 1\n");
+  int b = get_int("side 2\n");
+  int c = get_int("side 3\n");
+  int(sum) = (int)(a*a)+(b*b);
+   if((c*c) >= sum)
+   {
+    printf("Yes\n");
+   }
+   else
+   {
+    printf("No\n");
+   }
+}
+ 
